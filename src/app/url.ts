61d00b821593edcb8b7faa5470b1843e8d493c79
avaneeshp5-7 <avaneeshp507@gmail.com>
2019-08-13 11:34:37 +0530

    d

--- src/app/url.ts
+++ src/app/url.ts
@@ -1,9 +1,8 @@
 // var server='http://localhost:3000';
-var server='https://navyuvak-seva-samiti.herokuapp.com';
+ var server='https://navyuvak-seva-samiti.herokuapp.com';
 export const URL={
     post_file:server + '/post_file_url',
     get_file:server + '/gallery_url',
     get_single_image:server + '/single_image_url'
 }
-
 
